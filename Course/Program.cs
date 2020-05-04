2e28656f8e6326f25e5b327b478704b243fec931
Djalma <djalma.ant@gmail.com>
2020-05-04 19:06:17 -0300

    Method reference

--- Course/Program.cs
+++ Course/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Course.Entities;
 
 namespace Course
 {
@@ -6,7 +9,24 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            List<string> result = list.Select(NameUpper).ToList();
+            foreach (string s in result)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
         }
     }
 }
